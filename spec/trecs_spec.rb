ac4629b5e8ae7161fd65c85c82bba1719d9a1d25
Federico Iachetti <iachetti.federico@gmail.com>
2013-09-26 09:23:06 -0300

    CLI: Returns a the frame at a certain tima. Added convenience "trecs" helper method for testing

--- spec/trecs_spec.rb
+++ spec/trecs_spec.rb
@@ -3,36 +3,38 @@ require "fileutils"
 
 describe "T-Recs" do
   include FileUtils
+
+  let(:trecs_root)   { File.expand_path("../..", __FILE__) }
+  let(:bin)          { "#{trecs_root}/bin"    }
+  let(:exe)          { "#{bin}/trecs" }
+
+  let(:temp_dir)     { create_dir("#{trecs_root}/tmp")    }
+  let(:project_dir)  { create_dir("#{temp_dir}/project") }
+
   def create_dir(dir_path)
     rm_rf dir_path
     mkdir_p dir_path
     dir_path
   end
 
+  def trecs(*args)
+    command = [exe].concat(args.map(&:to_s))
+    IO.popen(command)
+  end
+
+
   context "trecs command" do
-    let(:trecs_root)   { File.expand_path("../..", __FILE__) }
-    let(:bin)          { "#{trecs_root}/bin"    }
-    let(:temp_dir)     { create_dir("#{trecs_root}/tmp")    }
-    let(:project_dir)  { create_dir("#{temp_dir}/project") }
-
-    let(:exe)          { "#{bin}/trecs" }
-
     specify "is in place and executes" do
-      expect { IO.popen(exe) }.not_to raise_exception
+      expect { trecs }.not_to raise_exception
     end
 
     context "Player" do
       specify "returns an error whe project doesn't exist" do
         file_name = "path/to/a/non/existing/file.txt"
 
-        command = []
-        command << exe
-        command << "-f"
-        command << "#{file_name}"
-        output = IO.popen(command)
+        output = trecs("-f", file_name)
 
         output.read.should == "File #{file_name} does not exist.\n"
-
       end
 
       specify "reads a one frame screencast" do
@@ -40,18 +42,32 @@ describe "T-Recs" do
         file_name = "#{project_dir}/#{file_basename}"
 
         File.open("#{project_dir}/0_#{file_basename}", File::WRONLY|File::CREAT) do |f|
-          f << "0"
+          f << "FIRST FRAME"
         end
+        output = trecs("-f", file_name)
 
-        command = []
-        command << exe
-        command << "-f"
-        command << "#{file_name}"
-        output = IO.popen(command)
-
-        output.read.should == "0\n"
+        output.read.should == "FIRST FRAME\n"
 
       end
+
+      specify "returns the frame at certain time" do
+        file_basename = "file.txt"
+        file_name = "#{project_dir}/#{file_basename}"
+
+        File.open("#{project_dir}/0_#{file_basename}", File::WRONLY|File::CREAT) do |f|
+          f << "FIRST FRAME"
+        end
+
+        File.open("#{project_dir}/100_#{file_basename}", File::WRONLY|File::CREAT) do |f|
+          f << "FRAME AT 100"
+        end
+
+        output = trecs("-f", file_name, "-t", 100)
+
+        output.read.should == "FRAME AT 100\n"
+
+      end
+
     end
   end
 end
