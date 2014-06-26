28d1753115afac79e182f99ad77abd217d6ae9ba
Federico Iachetti <iachetti.federico@gmail.com>
2014-06-26 16:46:10 -0300

    Made CommandStrategy more flexible

--- lib/trecs/recording_strategies/shell_command_strategy.rb
+++ lib/trecs/recording_strategies/shell_command_strategy.rb
@@ -4,8 +4,11 @@ module TRecs
    
     def current_content(str)
       if self.command
-        comm_array = command.split(" ")
-        str = IO.popen([*comm_array, "#{str}"]).read
+        #comm_array = command.split(" ")
+        #str = IO.popen([*comm_array, "#{str}"]).read
+
+        comm = command.gsub(/<frame>/, "\"#{str}\"")
+        str = IO.popen(comm).read
       end
       super(str)
     end
