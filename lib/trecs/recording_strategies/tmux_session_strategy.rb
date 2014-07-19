03bd9f50fb7db9880efcf659b7d263076afb9d19
Federico Iachetti <iachetti.federico@gmail.com>
2014-07-19 19:20:43 -0300

    Can turn off color on tmux sessions

--- lib/trecs/recording_strategies/tmux_session_strategy.rb
+++ lib/trecs/recording_strategies/tmux_session_strategy.rb
@@ -8,12 +8,15 @@ module TRecs
     def initialize(options={})
       @frames = []
       @step = options.fetch(:step) { 100 } / 1000.0
+      @color = options.fetch(:color) { true }
+      set_color_or_bw
     end
 
     def perform
       start_recording
       while(recording)
-        system *%W[tmux capture-pane -e]
+        command
+        system *command
         IO.popen(%W[tmux show-buffer]) do |out|
           recorder.current_frame(content: out.read)
         end
@@ -21,13 +24,28 @@ module TRecs
       end
     end
 
+    def command
+      @command ||=
+        set_command
+    end
+
+    def set_command
+      command = %W[tmux capture-pane]
+      command << @color if @color
+      command
+    end
+
     def stop
       @recording = false
     end
+
     private
     def start_recording
       @recording = true
     end
 
+    def set_color_or_bw
+      @color = @color ? "-e" : nil
+    end
   end
 end
