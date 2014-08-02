401173a03a56655d13af6c1976771bc49f592194
Federico Iachetti <iachetti.federico@gmail.com>
2014-08-02 03:07:42 -0300

    Added File Frames Strategy

---
+++ lib/trecs/recording_strategies/file_frames_strategy.rb
@@ -0,0 +1,30 @@
+require "recording_strategies/strategy"
+module TRecs
+  class FileFramesStrategy
+    include Strategy
+    attr_accessor :frames
+
+    def initialize(dirname)
+      @frames = Dir.entries(dirname)
+        .select { |d| d =~ /\d+/}
+        .each_with_object(Hash.new) do |filename, h|
+        h[filename.to_i] = File.read(dirname + "/" + filename)
+      end
+    end
+
+    def perform
+      @frames.each do |time, content|
+        current_time(time)
+        current_content(content)
+        save_frame
+      end
+    end
+
+    def stop
+    end
+
+    def inspect
+      "<#{self.class}: frames: #{frames}>"
+    end
+  end
+end
---
+++ spec/trecs/recording_strategies/file_frames_strategy_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+require "recording_strategies/file_frames_strategy"
+
+module TRecs
+  describe FileFramesStrategy do
+    context "initialization" do
+      Then { skip("Test me!") }
+    end
+  end
+end
