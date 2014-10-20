5b75c81ac25b0925a5dd85310f9c7c90da98b221
Federico Iachetti <iachetti.federico@gmail.com>
2014-10-20 19:34:35 -0300

    Updated Strategy

--- lib/trecs/strategies/strategy.rb
+++ lib/trecs/strategies/strategy.rb
@@ -1,6 +1,17 @@
+require "frame"
 module TRecs
   module Strategy
     attr_accessor :recorder
+    attr_accessor :offset
+
+    def write_frames_to(writer)
+      perform
+      writer.render_frames(frames)
+    end
+
+    def frames
+      @frames ||= {}
+    end
 
     def stop
     end
@@ -24,19 +35,16 @@ module TRecs
       end
       @__content
     end
- 
+
     def current_format(format=nil)
       if format
         @__format = format
       end
       @__format
     end
-    
-    def save_frame
-      options = {time: __time, format: __format, content: __content}
-      options.reject! {|k,v| v.nil?}
 
-      recorder.current_frame(options)
+    def save_frame
+      frames[__time] = Frame.new(format: __format, content: __content)
     end
   end
 end
---
+++ spec/trecs/strategies/strategy_spec.rb
@@ -0,0 +1,46 @@
+require "spec_helper"
+require "strategies/strategy"
+
+module TRecs
+  class MyStrategy
+    include Strategy
+    attr_accessor :action
+
+    def perform
+      current_time(0)
+      current_content("A")
+      current_format("raw")
+      save_frame
+
+      current_time(10)
+      current_content("<p>B</p>")
+      current_format("html")
+      save_frame
+
+      current_time(50)
+      current_content("C")
+      save_frame
+    end
+  end
+
+  describe Strategy do
+    Given(:strategy) { MyStrategy.new }
+
+    context "recorder" do
+      When { strategy.recorder = :recorder }
+      Then { strategy.recorder == :recorder }
+    end
+
+    context "performing" do
+      Given(:writer)   { InMemoryWriter.new }
+      When { strategy.write_frames_to(writer) }
+
+      Then { writer.frames[0].content == "A" }
+      Then { writer.frames[0].format  == "raw" }
+
+      Then { writer.frames[10].content == "<p>B</p>" }
+      Then { writer.frames[10].format  == "html" }
+    end
+
+  end
+end
