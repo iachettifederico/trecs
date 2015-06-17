7f3dfe5079b133ce8a64329037f3f8da41de371e
Federico Iachetti <iachetti.federico@gmail.com>
2015-06-16 21:42:19 -0300

    Added a test for swipe strategy

--- spec/trecs/strategies/swipe_strategy_spec.rb
+++ spec/trecs/strategies/swipe_strategy_spec.rb
@@ -12,31 +12,31 @@ module TRecs
       When(:strategy) { SwipeStrategy.new }
       Then { expect(strategy).to have_raised(/key not found.*message/) }
     end
-    
+
     context "performing" do
       Given(:writer)   { InMemoryWriter.new }
       Given(:recorder) { Recorder.new(strategy: strategy, writer: writer) }
-    
+
       When { strategy.recorder = recorder }
       When { recorder.record }
-    
+
       context "one char" do
         Given(:strategy) { SwipeStrategy.new(message: "a") }
-        Then { writer.frames[0]   == "*" }
-        Then { writer.frames[100] == "|" }
+        Then { writer.frames[0]   == "*"  }
+        Then { writer.frames[100] == "|"  }
         Then { writer.frames[200] == "a|" }
-        Then { writer.frames[300] == "a" }
+        Then { writer.frames[300] == "a"  }
       end
-    
+
       context "two chars" do
         Given(:strategy) { SwipeStrategy.new(message: "ab", step: 10) }
-        Then { writer.frames[0]  == "**" }
-        Then { writer.frames[10] == "|*" }
-        Then { writer.frames[20] == "a|" }
+        Then { writer.frames[0]  == "**"  }
+        Then { writer.frames[10] == "|*"  }
+        Then { writer.frames[20] == "a|"  }
         Then { writer.frames[30] == "ab|" }
-        Then { writer.frames[40] == "ab" }
+        Then { writer.frames[40] == "ab"  }
       end
-    
+
       # context "multiple_lines" do
       #   Given(:strategy) { SwipeStrategy.new(message: "FIRST\nSECOND") }
       #   Then { writer.frames[0]   == "******\n******" }
@@ -49,22 +49,30 @@ module TRecs
       #   Then { writer.frames[700] == "FIRST |\nSECOND|" }
       #   Then { writer.frames[800] == "FIRST\nSECOND" }
       # end
-    
+
       context "swiper and hider" do
         context "swiper" do
           Given(:strategy) { SwipeStrategy.new(message: "a", swiper: ">") }
-          Then { writer.frames[0]   == "*" }
-          Then { writer.frames[100] == ">" }
+          Then { writer.frames[0]   == "*"  }
+          Then { writer.frames[100] == ">"  }
           Then { writer.frames[200] == "a>" }
-          Then { writer.frames[300] == "a" }
+          Then { writer.frames[300] == "a"  }
         end
-    
+
         context "hider" do
           Given(:strategy) { SwipeStrategy.new(message: "a", hider: "#") }
-          Then { writer.frames[0]   == "#" }
-          Then { writer.frames[100] == "|" }
+          Then { writer.frames[0]   == "#"  }
+          Then { writer.frames[100] == "|"  }
           Then { writer.frames[200] == "a|" }
-          Then { writer.frames[300] == "a" }
+          Then { writer.frames[300] == "a"  }
+        end
+
+        context "swiper and hider" do
+          Given(:strategy) { SwipeStrategy.new(message: "a", hider: "+", swiper: "}") }
+          Then { writer.frames[0]   == "+"  }
+          Then { writer.frames[100] == "}"  }
+          Then { writer.frames[200] == "a}" }
+          Then { writer.frames[300] == "a"  }
         end
       end
     end
